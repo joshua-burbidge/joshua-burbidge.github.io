99d5df0aeb6d06b5a62764f8f2b594a2c7ca2e6e
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-10-12 11:55:49 -0500

    add image to card

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,8 +4,8 @@ import OtherSection from './OtherSection';
 
 function Home() {
   return (
-    <Container centerContent height={'2000px'}>
-      <VStack spacing={8} divider={<StackDivider/>}>
+    <Container centerContent height={'2000px'} maxWidth={'container.md'}>
+      <VStack spacing={8} divider={<StackDivider/>} width={'100%'}>
         <Profile/>
         <Text>{'Hello there this a some text in a Container'}</Text>
         <Button variant={'solid'}>
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,14 +1,21 @@
-import { Card, CardBody, Container, Text } from "@chakra-ui/react";
+import { Box, Card, Image, Stack, Text } from "@chakra-ui/react";
+import imgUrl from '../resources/profile.jpg';
 
 function Profile() {
   return (
-    <Container scrollMarginTop={'var(--header-height)'} id={'profile'} paddingY={12}>
-      <Card>
-        <CardBody>
+    <Box scrollMarginTop={'var(--header-height)'} id={'profile'} paddingY={12}>
+      <Card direction={'row'} borderRadius={'lg'}>
+        <Image src={imgUrl}></Image>
+        <Stack padding={4}>
+          <Box>
+            <Text fontSize={'4xl'} fontWeight={'light'}>{'Joshua Burbidge'}</Text>
+            <Text fontSize={'md'} fontWeight={600} marginTop={'-10px'}>{'Software Developer'}</Text>
+          </Box>
           <Text>{'Card'}</Text>
-        </CardBody>
+          <Text>{'Card'}</Text>
+        </Stack>
       </Card>
-    </Container>
+    </Box>
   );
 }
 
