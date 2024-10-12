a4fbaa250e70d787911a098c5a9069d01dab2764
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-10-12 10:50:46 -0500

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from './Home';
 import Header from './Header';
+import './main.css';
 
 function App() {
   return (
