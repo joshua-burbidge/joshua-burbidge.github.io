b62f471c3da4ffbe44da6c995e16e3f608ced5a0
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-11-17 07:39:45 -0600

    update image

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,11 +1,12 @@
 import { Box, Card, Image, Stack, Text } from "@chakra-ui/react";
-import imgUrl from '../resources/profile.jpg';
+// import imgUrl from '../resources/profile.jpg';
+import newImg from '../resources/profile-edited-2.jpeg';
 
 function Profile() {
   return (
     <Box scrollMarginTop={'var(--header-height)'} id={'profile'} paddingY={12}>
       <Card direction={'row'} borderRadius={'lg'}>
-        <Image src={imgUrl}></Image>
+        <Image src={newImg} height={'200px'} borderLeftRadius={'lg'}></Image>
         <Stack padding={4}>
           <Box>
             <Text fontSize={'4xl'} fontWeight={'light'}>{'Joshua Burbidge'}</Text>
